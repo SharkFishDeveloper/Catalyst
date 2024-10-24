52c02cb2a1509aa4a6f2c50f20bb3435d172449b
Shahzeb Akhtar <shahzebakhtar66@gmail.com>
2024-10-25 01:08:55 +0530

    pm2 chantes

--- java-engine/app/Main.java
+++ java-engine/app/Main.java
@@ -1,22 +1,34 @@
 
             public class Main {
                 public static void main(String[] args) {
-                    int num11 = 1;
-int num21 = 3;
-int output1 = add(num11,num21);
+                    int num11 = 2;
+int num21 = 2;
+int output1 = divide(num11,num21);
 System.out.println(output1);
- int num12 = 4;
-int num22 = 5;
-int output2 = add(num12,num22);
+ int num12 = 2;
+int num22 = -1;
+int output2 = divide(num12,num22);
 System.out.println(output2);
+ int num13 = 3;
+int num23 = 2;
+int output3 = divide(num13,num23);
+System.out.println(output3);
+ int num14 = 2;
+int num24 = 0;
+int output4 = divide(num14,num24);
+System.out.println(output4);
+ int num15 = 100;
+int num25 = 1;
+int output5 = divide(num15,num25);
+System.out.println(output5);
 
 
                 }
                 
 //Add imports here
-    public static int add(int num1, int num2) {
-    // Your code
-    return num1+num2;
+    public static int divide(int num1, int num2) {
+    if(num2 == 0)return -1;
+    return num1/num2;
 }
             }
         
--- js-engine/app/index.js
+++ js-engine/app/index.js
@@ -1,16 +1,25 @@
 
 //Add imports here
-    function add (num1, num2) {
+    function divide (num1, num2) {
     // Your code
-    return num1+num2;
+    return 1;
 }
 function main() {
     
-        const result1 = add(1, 3);
+        const result1 = divide(2, 2);
         console.log(result1);
     
-        const result2 = add(4, 5);
+        const result2 = divide(2, -1);
         console.log(result2);
+    
+        const result3 = divide(3, 2);
+        console.log(result3);
+    
+        const result4 = divide(2, 0);
+        console.log(result4);
+    
+        const result5 = divide(100, 1);
+        console.log(result5);
 }
 
 main();
