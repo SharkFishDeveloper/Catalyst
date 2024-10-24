d37458a108270b2eaae47dbaa785ddfbc7872d09
Shahzeb Akhtar <shahzebakhtar66@gmail.com>
2024-10-25 00:13:43 +0530

    moedifeid scriopt

--- js-engine/app/index.js
+++ js-engine/app/index.js
@@ -1,15 +1,15 @@
 
 //Add imports here
-    function mul (num1, num2) {
+    function add (num1, num2) {
     // Your code
-    return num1*num2;
+    return num1+num2;
 }
 function main() {
     
-        const result1 = mul(12, 100);
+        const result1 = add(1, 3);
         console.log(result1);
     
-        const result2 = mul(100, 56);
+        const result2 = add(4, 5);
         console.log(result2);
 }
 
